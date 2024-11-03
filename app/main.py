e0c0f551703c848b8b38b0a4ad9c16f743bff982
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2024-11-03 15:47:58 -0500

    Add title, description, and version for server

--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ from fastapi import FastAPI
 from .routes import router
 
 
-app = FastAPI()
+app = FastAPI(
+    title="QInvst Automation Tool for Predictive Forecast",
+    description="This is a tool to automate the financial data extraction, competitor analysis, and revenue forecasting for a target company.",
+    version="0.1.0"
+
+)
 
 app.include_router(router)
-
-
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to the QInvst Automation Tool!"}
