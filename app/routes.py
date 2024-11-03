354e9e9abcb59e541d62a1c4d01458d09dcbca4a
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2024-11-03 15:49:26 -0500

    Logic for forecast API to perform predictive analysis

--- app/routes.py
+++ app/routes.py
@@ -1,39 +1,27 @@
-from fastapi import APIRouter, File, UploadFile
-import shutil
-
-import pandas as pd
-from .services.parser import extract_data_from_excel, extract_text_from_pdf, find_financial_data
-from .services.industry_classifier import find_competitors
-from .services.fin_data_fetcher import competitors_quarterly_revenue, export_to_excel
+from fastapi import APIRouter, Response, HTTPException
+from app.models import ForecastRequest
+from app.services.forecasting import generate_forecasting_prompt, generate_predictive_analysis
 
 
 router = APIRouter()
 
 
-@router.post("/upload/")
-async def upload_file(file: UploadFile = File(...)):
-    file_location = f"uploaded_files/{file.filename}"
-    with open(file_location, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
-
-    content = None
-    if file.filename.endswith(".pdf"):
-        content = extract_text_from_pdf(file_location)
-
-    fin_data = find_financial_data(content)
-
-    return {"filename": file.filename, "financial_data": fin_data}
+@router.get("/")
+async def read_root():
+    return {"message": "Welcome to QInvst Automation Tool for Predictive Forecasting"}
 
 
-@router.post("/comp_revenue")
-async def classify(url: str):
-    # Find Competitors
-    competitors_ticker_symbols: list = find_competitors(url)
+@router.post("/forecast")
+async def forecast(request: ForecastRequest):
+    if (request.website_url is None):
+        raise HTTPException(
+            status_code=400, detail="Website URL or Company Description are required to forecast revenue.")
 
-    # Get quarterly revenue data
-    combined_revenue_data: pd.DataFrame = competitors_quarterly_revenue(
-        competitors_ticker_symbols)
+    try:
+        prompt = generate_forecasting_prompt(request)
+        forecast = generate_predictive_analysis(prompt)
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"An error occurred while generating the forecast: {e}")
 
-    # Export data to Excel sheet
-    export_to_excel(combined_revenue_data)
-    return
+    return Response(forecast, media_type="text/markdown", headers={"Content-Disposition": "attachment; filename = report.md"})
